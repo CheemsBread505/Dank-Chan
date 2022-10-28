06901f2e70da80e90dd4feae084d2f61e2c242bc
CheemsBread505 <CheemsBread505@proton.me>
2022-10-28 16:41:27 -0400

    Fixed formatting
    Fixed formatting in the text box of posts

--- b/post.php
+++ b/post.php
@@ -3,7 +3,7 @@ $random = (rand());
 
 if(isset($_POST['uname']) && isset($_POST['textBox']) && isset($_POST['subject'])) {
     $subjectHeader = $_POST['subject'];
-    $data = '<div class="postbox'.$random.'">'. '<h3>'.$subjectHeader.'</h3>'. '<p>'.$_POST['uname'].'</p>'. '<p>'.$_POST['textBox'].'</p>'. '</div>'. "\r\n";
+    $data = '<div class="postbox'.$random.'">'. '<h3>'.$subjectHeader.'</h3>'. '<p>'.$_POST['uname'].': '.$random.'</p>'. '<hr>'. '<p>'.$_POST['textBox'].'</p>'. '</div>'. "\r\n";
     $ret = file_put_contents('chat.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
