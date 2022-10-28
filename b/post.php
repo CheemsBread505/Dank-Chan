0686d2439a8669aa3e9fff68c3d693a5302b23ee
CheemsBread505 <CheemsBread505@proton.me>
2022-10-28 16:23:01 -0400

    Got posting to work
    I got posting working. users now can post on the site.
    images and reply will be added soon.

--- b/post.php
+++ b/post.php
@@ -1,6 +1,9 @@
 <?php
-if(isset($_POST['uname']) && isset($_POST['textBox'])) {
-    $data = '<div class="postbox">'. $_POST['uname']. $_POST['textBox']. '</div>'. "\r\n";
+$random = (rand());
+
+if(isset($_POST['uname']) && isset($_POST['textBox']) && isset($_POST['subject'])) {
+    $subjectHeader = $_POST['subject'];
+    $data = '<div class="postbox'.$random.'">'. '<h3>'.$subjectHeader.'</h3>'. '<p>'.$_POST['uname'].'</p>'. '<p>'.$_POST['textBox'].'</p>'. '</div>'. "\r\n";
     $ret = file_put_contents('chat.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
